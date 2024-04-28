9f4a97b316d4f582ba9812719a6d81b518d6a20e
cocoyoon <socozy7210@gmail.com>
2024-04-28 16:47:45 +0900

    rennovate

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { FirebaseHelper } from "../../common/firebase";
 import { listAll, getDownloadURL, getMetadata } from "firebase/storage";
+import { custom_font } from "@/components/helpers/util";
 
 const fetchAllImages = async (point: string) => {
   try {
@@ -30,12 +31,71 @@ const fetchAllImages = async (point: string) => {
   }
 };
 
-async function HomePage() {
+async function Home() {
+  return (
+    <div>
+      <NewTaggedSection />
+      <NewsSection />
+      <SpotlightSection />
+    </div>
+  );
+}
+
+// async function HomePage() {
+//   const urlsAndHashes = await fetchAllImages("images");
+//   return (
+//     <div className="grid sm:grid-cols-3 md:grid-cols-4 gap-6 max-w-[95%] w-full mx-auto mt-10">
+//       {urlsAndHashes?.map(({ url, hash }) => (
+//         <div key={hash} className="rounded-lg relative aspect-w-3 aspect-h-5">
+//           <Link
+//             href={`images/${hash}?imageUrl=${encodeURIComponent(url)}`}
+//             prefetch={false}
+//           >
+//             <Image
+//               alt="LOADING"
+//               className="w-full h-auto rounded-lg"
+//               layout="fill"
+//               src={url}
+//               quality={80}
+//               objectFit="cover"
+//               placeholder="blur"
+//               blurDataURL="data:image/jpeg;base64,..."
+//               sizes="100vw"
+//             />
+//           </Link>
+//         </div>
+//       )) || (
+//         <div>
+//           <span className="loading loading-infinity loading-md"></span>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+async function NewTaggedSection() {
+  return (
+    <div className="h-full rounded-md border-l-2 border-r-2 border-b-2 border-black p-3">
+      <div className="flex justify-start items-stretch h-full">
+        <div className="flex flex-col w-full h-full">
+          <h1 className="text-2xl font-bold">Today's Pick</h1>
+          <h2 className="text-lg font-bold">Description </h2>
+        </div>
+        <ImageCarousel />
+      </div>
+    </div>
+  );
+}
+
+async function ImageCarousel() {
   const urlsAndHashes = await fetchAllImages("images");
   return (
-    <div className="grid sm:grid-cols-3 md:grid-cols-4 gap-6 max-w-[95%] w-full mx-auto mt-10">
-      {urlsAndHashes?.map(({ url, hash }) => (
-        <div key={hash} className="rounded-lg relative aspect-w-3 aspect-h-5">
+    <div className="w-60 carousel rounded-box mx-5" style={{ width: "70vw" }}>
+      {urlsAndHashes.map(({ url, hash }) => (
+        <div
+          key={hash}
+          className="carousel-item w-full relative aspect-w-5 aspect-h-6"
+        >
           <Link
             href={`images/${hash}?imageUrl=${encodeURIComponent(url)}`}
             prefetch={false}
@@ -53,13 +113,91 @@ async function HomePage() {
             />
           </Link>
         </div>
-      )) || (
-        <div>
-          <span className="loading loading-infinity loading-md"></span>
-        </div>
-      )}
+      ))}
     </div>
   );
 }
 
-export default HomePage;
+async function NewsSection() {
+  return (
+    <div className="rounded-md border-l-2 border-r-2 border-black p-3">
+      <h1 className={`${custom_font.className} text-2xl font-bold mb-4`}>
+        LAST NEWS
+      </h1>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="news-item">
+          <img
+            src="path/to/image1.jpg"
+            alt="News Image 1"
+            className="w-full h-auto"
+          />
+          <div className="text-sm bg-black text-white p-2">
+            HYPEBEAST 디스인증페배이 '로텐덤' 브랜드를 출시했다
+          </div>
+        </div>
+        <div className="news-item">
+          <img
+            src="path/to/image2.jpg"
+            alt="News Image 2"
+            className="w-full h-auto"
+          />
+          <div className="text-sm bg-black text-white p-2">
+            슈프림 '30주년 기념 티셔츠 1994-2024' 출시
+          </div>
+        </div>
+        <div className="news-item">
+          <img
+            src="path/to/image3.jpg"
+            alt="News Image 3"
+            className="w-full h-auto"
+          />
+          <div className="text-sm bg-black text-white p-2">
+            루이 비통, 파페 VIA 바스티예 채널 디자인한 백주 공개
+          </div>
+        </div>
+      </div>
+      <button className="w-full text-align-center mt-4 py-2 px-6 bg-red-700 text-white rounded">
+        See More
+      </button>
+    </div>
+  );
+}
+
+async function SpotlightSection() {
+  return (
+    <div className="rounded-md border-2 border-black p-3 bg-black">
+      <div className="text-center mb-6">
+        <h1 className="text-4xl font-bold">SPOTLIGHT</h1>
+        <h2 className="text-2xl font-bold">JENNIE 24SS</h2>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="col-span-1 md:col-span-3">
+          <img
+            src="path/to/jennie-main.jpg"
+            alt="Jennie Main"
+            className="w-full"
+          />
+        </div>
+        <div className="col-span-1">
+          <img src="path/to/product.jpg" alt="Product" className="w-full" />
+          <p className="text-center mt-2">
+            Baby Fox Patch Cardigan
+            <br />
+            $415
+          </p>
+        </div>
+        <div className="col-span-1">
+          <img src="path/to/jennie-1.jpg" alt="Jennie 1" className="w-full" />
+        </div>
+        <div className="col-span-1">
+          <img src="path/to/jennie-2.jpg" alt="Jennie 2" className="w-full" />
+        </div>
+        <div className="col-span-1">
+          <img src="path/to/jennie-3.jpg" alt="Jennie 3" className="w-full" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,49 +3,83 @@
 import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { custom_font } from "./helpers/util";
-import Search from "./Search";
+import { useState } from "react";
+import { MagnifyingGlassIcon, Bars3Icon } from "@heroicons/react/20/solid";
+import { custom_font } from "@/components/helpers/util";
 
 function Header() {
+  return (
+    <header className="grid grid-cols-3 items-center rounded-b-md border-2 border-black">
+      <LogoSection />
+      <MenuSection />
+      <SearchSection />
+    </header>
+  );
+}
+
+function LogoSection() {
+  return (
+    <Link
+      href="/"
+      prefetch={false}
+      className={`${custom_font.className} text-2xl font-bold`}
+    >
+      TAGGED
+    </Link>
+  );
+}
+
+function MenuSection() {
   const path = usePathname();
   const homeOpacity = path === "/" ? "text-opacity-100" : "text-opacity-50";
   const newsOpacity = path === "/news" ? "text-opacity-100" : "text-opacity-50";
+  return (
+    <nav className="w-full justify-items-center">
+      <ul className="flex justify-center gap-20">
+        <li className="list-none transition-all duration-100 ease-in-out hover:scale-150">
+          <Link
+            href="/"
+            prefetch={false}
+            className={`font-bold text-m ${homeOpacity} ${custom_font.className}`}
+          >
+            HOME
+          </Link>
+        </li>
+        <li className="list-none transition-all duration-100 ease-in-out hover:scale-150">
+          <Link
+            href="/news"
+            prefetch={false}
+            className={`font-bold text-m ${newsOpacity} ${custom_font.className}`}
+          >
+            NEWS
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+function SearchSection() {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleInputChange = (event: any) => {
+    setSearchValue(event.target.value);
+  };
+
+  const clearSearch = () => {
+    setSearchValue("");
+  };
 
   return (
-    <header className="flex flex-col items-center fixed w-full sm:rounded-lg py-5 top-0 z-50 bg-white">
-      <div className="flex justify-between w-full items-center px-5 ">
-        <Link
-          href="/"
-          prefetch={false}
-          className={`${custom_font.className} text-2xl font-bold text-black`}
-        >
-          TAGGED
-        </Link>
-        <Search />
-      </div>
-      <nav className="w-full mt-5 justify-items-center">
-        <ul className="flex justify-center gap-20">
-          <li className="list-none transition-all duration-100 ease-in-out hover:scale-150">
-            <Link
-              href="/"
-              prefetch={false}
-              className={`font-bold text-black text-xl ${homeOpacity} ${custom_font.className}`}
-            >
-              HOME
-            </Link>
-          </li>
-          <li className="list-none transition-all duration-100 ease-in-out hover:scale-150">
-            <Link
-              href="/news"
-              prefetch={false}
-              className={`font-bold text-black text-xl ${newsOpacity} ${custom_font.className}`}
-            >
-              NEWS
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <button
+      className="flex justify-end items-center"
+      onClick={() => {
+        alert("WIP");
+      }}
+    >
+      <MagnifyingGlassIcon className="w-4 h-4" />
+      <p className={`mx-1 ${custom_font.className} text-sm`}>SEARCH</p>
+    </button>
   );
 }
 
