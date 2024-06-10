3ed0284afa414c1d986a6d5d47bd04e1f915d190
cocoyoon <socozy7210@gmail.com>
2024-06-10 11:51:20 +0900

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,47 +1,51 @@
 "use client";
+import { useState } from "react";
 import { main_font, secondary_font } from "./helpers/util";
 
 function Footer() {
+  const [isType, setIsType] = useState<boolean>(false);
+
   return (
     <footer className="bg-[#ffffff] border-2 border-black rounded-md">
-      <div className="flex justify-center items-center">
-        <div className="flex flex-col items-center mb-20">
+      <div className="grid grid-cols-1 justify-center items-center">
+        <div className="flex flex-col items-center">
           <div className="flex flex-col items-center mb-10">
             <h1
-              className={`font-bold ${main_font.className} text-5xl p-5 dark:text-[#000000] text-[#FF204E]`}
+              className={`font-bold ${main_font.className} text-4xl md:text-6xl dark:text-[#000000] text-[#FF204E]`}
             >
-              "TAGGED Delivery Service"
+              "Delivery Service"
             </h1>
             <h2
-              className={`${secondary_font.className} text-xl opacity-50 dark:text-[#000000]`}
+              className={`${secondary_font.className} text-lg md:text-2xl opacity-50 text-[#000000] dark:text-[#ffffff]`}
             >
               "Unsubscribe any time"
             </h2>
           </div>
-          <form className="flex flex-col items-center">
-            <div className="flex items-center">
+          <form className="flex flex-col items-center w-full">
+            <div className="flex items-center w-[50%] mb-10">
               <input
                 type="email"
-                placeholder="Write Your Email"
-                className="p-2 border-b border-[#FF204E] w-[500px] dark:bg-[#ffffff] focus:outline-none text-black dark:text-white"
-              />
-              <button
-                type="submit"
-                className="p-1 bg-[#FF204E] text-white rounded-md mx-2"
-                onClick={() => {
-                  alert("WIP");
+                placeholder="*@*.com"
+                className="w-full border-b-2 border-[#FF204E] dark:bg-[#ffffff] focus:outline-none text-black dark:text-white"
+                onChange={(e) => {
+                  if (e.target.value.length > 0) {
+                    setIsType(true);
+                  } else {
+                    setIsType(false);
+                  }
                 }}
-              >
-                →
-              </button>
+              />
+              {isType ? (
+                <button
+                  type="submit"
+                  className="p-1 bg-[#FF204E] text-white rounded-md mx-2"
+                >
+                  →
+                </button>
+              ) : null}
             </div>
           </form>
         </div>
-      </div>
-      <div
-        className={`${main_font.className} text-center font-bold rounded-md p-4 text-xl text-black`}
-      >
-        © 2023 TAGGED PLATFORM
       </div>
     </footer>
   );
