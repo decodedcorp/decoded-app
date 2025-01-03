caf08eaaec7d5336a82a105583d45fa1412d3ef0
cocoyoon <socozy7210@gmail.com>
2025-01-03 13:48:18 +0900

    add interfaces

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,14 +1,12 @@
-'use client';
+"use client";
 
-import { HeroCarousel } from './components/sections/hero-carousel';
-import { DirectorPicks } from './components/sections/director-pick';
-import { DiscoverSection } from './components/sections/discover';
-import { TrendingKeywords } from './components/sections/trending';
+import { DirectorPicks } from "./components/sections/director-pick";
+import { DiscoverSection } from "./components/sections/discover";
+import { TrendingKeywords } from "./components/sections/trending";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-[100vh]">
-      <HeroCarousel />
       <DirectorPicks />
       <DiscoverSection />
       <TrendingKeywords />
